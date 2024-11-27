449cfb511aa52ad5725766af6f85d92f3275c2c5
cslj <cslj@itu.dk>
2024-11-27 16:44:35 +0100

    Send bid, added bidders

--- Server/main.go
+++ Server/main.go
@@ -1,6 +1,9 @@
 package Server
 
 import (
+	"fmt"
+	"log"
+
 	api "github.com/DiSysCBFA/Handind-5/Api"
 )
 
@@ -9,6 +12,8 @@ type Server struct {
 	Port                 string
 	CurrentHighestBid    int64
 	CurrentHighestBidder string
+	AuctionStatus        string
+	Bidders              map[string]int64
 }
 
 func NewServer(port string) *Server {
@@ -17,10 +22,33 @@ func NewServer(port string) *Server {
 	}
 }
 
-func SendBid(bid *api.Bid) (*api.BidAck, error) {
-	return &api.BidAck{Ack: "Bid Accepted"}, nil
+func (s *Server) SendBid(bid *api.Bid) (*api.BidAck, error) {
+
+	// Checking if the auction is ongoing
+	if s.AuctionStatus == "ENDED" {
+		return &api.BidAck{Ack: "Auction has ended. No more bids allowed."}, nil
+	}
+
+	// checking the bid amount
+	if bid.Bid <= s.CurrentHighestBid {
+		return &api.BidAck{Ack: fmt.Sprintf("Bid too low. Current highest bid is %d.", s.CurrentHighestBid)}, nil
+	}
+
+	// Register the bidder if it's their first bid
+	if _, exists := s.Bidders[bid.Bidder]; !exists {
+		s.Bidders[bid.Bidder] = bid.Bid
+	}
+
+	// Update the auction state
+	s.CurrentHighestBid = bid.Bid
+	s.CurrentHighestBidder = bid.Bidder
+	log.Printf("New highest bid: %d by %s", bid.Bid, bid.Bidder)
+
+	// Return if success
+	return &api.BidAck{Ack: "Bid accepted"}, nil
 }
 
-func JoinAuction(stream api.Auctionservice_JoinAuctionServer) error {
+func (s *Server) JoinAuction(stream api.Auctionservice_JoinAuctionServer) error {
 	return nil
+
 }
