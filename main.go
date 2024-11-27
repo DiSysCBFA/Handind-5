c93cf54599488319347ac6fcff99ba185c664700
cslj <cslj@itu.dk>
2024-11-27 10:47:52 +0100

    added file reader for ports

--- main.go
+++ main.go
@@ -1,25 +1,55 @@
 package main
 
 import (
+	"bufio"
 	"log"
+	"os"
+	"strconv"
+	"strings"
 
 	"github.com/manifoldco/promptui"
 )
 
 func main() {
-	selection := promptui.Select{
-		Label: "Select an option",
-		Items: []string{"Start Server", "Start new Client", "Exit"},
-	}
-
-	_, result, err := selection.Run()
+	file, err := os.Open("ports.txt")
 	if err != nil {
-		log.Fatalf("Failed to run: %v", err)
+		log.Fatal(err)
+		return
 	}
 
-	if result == "Start Server" {
-		// Start the server
-	} else if result == "Start new Client" {
-		// Start a new client
+	r := bufio.NewReader(file)
+
+	nop, err := r.ReadString('\n')
+	var numberOfPeers, _ = strconv.Atoi(strings.TrimSpace(nop))
+	log.Println("Number of peers is: ", numberOfPeers)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	for i := 0; i < numberOfPeers; i++ {
+		port, err := r.ReadString('\n') //! Make sure last line has a new line
+		log.Println(port)
+		if err != nil {
+			break
+		}
+
+		port = strings.TrimSpace(port)
+		log.Println(port) // Displaying each port read
+
+		selection := promptui.Select{
+			Label: "Select an option",
+			Items: []string{"Start Server", "Start new Client", "Exit"},
+		}
+
+		_, result, err := selection.Run()
+		if err != nil {
+			log.Fatalf("Failed to run: %v", err)
+		}
+
+		if result == "Start Server" {
+			// Start the server
+		} else if result == "Start new Client" {
+			// Start a new client
+		}
 	}
 }
