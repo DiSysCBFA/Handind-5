50816b3dd3391305f1dc802be3c206b8f7bc8445
cslj <cslj@itu.dk>
2024-11-27 11:26:16 +0100

    skabelon

--- Server/main.go
+++ Server/main.go
@@ -1,1 +1,12 @@
 package Server
+
+import (
+	api "github.com/DiSysCBFA/Handind-5/Api"
+)
+
+type Server struct {
+	api.UnimplementedAuctionserviceServer
+	Port                 string
+	CurrentHighestBid    int64
+	CurrentHighestBidder string
+}
