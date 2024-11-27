8b75fc97404337218ad16f7df72a6e1100a94d5f
polu <polu@itu.dk>
2024-11-27 10:17:32 +0100

    added selection prompt

--- main.go
+++ main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"log"
+
+	"github.com/manifoldco/promptui"
+)
+
+func main() {
+	selection := promptui.Select{
+		Label: "Select an option",
+		Items: []string{"Start Server", "Start new Client", "Exit"},
+	}
+
+	_, result, err := selection.Run()
+	if err != nil {
+		log.Fatalf("Failed to run: %v", err)
+	}
+
+	if result == "Start Server" {
+		// Start the server
+	} else if result == "Start new Client" {
+		// Start a new client
+	}
+}
