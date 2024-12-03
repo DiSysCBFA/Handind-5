4045973df85111e11f7d92bc3f5a2ba7179e973f
polu <polu@itu.dk>
2024-12-03 23:14:16 +0100

    retreiveResult

--- Client/main.go
+++ Client/main.go
@@ -1,15 +1,21 @@
 package client
 
 import (
+	"context"
 	"log"
 	"os"
+	"reflect"
 	"sync"
 
+	h5 "github.com/DiSysCBFA/Handind-5/Api"
 	"github.com/manifoldco/promptui"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
-func StartClient(wg *sync.WaitGroup, ports []int) {
+func StartClient(wg *sync.WaitGroup, ports []string, id string) {
 	defer wg.Done()
+	client := h5.NewAuctionserviceClient(nil)
 	for {
 		selectAction := promptui.Select{
 			Label: "Select an option",
@@ -21,10 +27,48 @@ func StartClient(wg *sync.WaitGroup, ports []int) {
 		}
 
 		if selectedAction == "Bid" {
+			getBid(client)
 		} else if selectedAction == "Result" {
+			retreiveResult(ports)
 		} else if selectedAction == "Exit" {
 			log.Println("Exiting...")
 			os.Exit(0)
 		}
 	}
+}
+
+func retreiveResult(ports []string) {
+	responses := [3]*h5.AuctionResult{}
+	for _, port := range ports {
+		log.Println("Dialing", port)
+		conn, err := grpc.NewClient(port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			log.Fatalf("Failed to dial: %v. Attempting next port", err)
+			continue
+		}
+
+		empty := &h5.Empty{}
+
+		client := h5.NewAuctionserviceClient(conn)
+		response, err := client.JoinAuction(context.Background(), empty)
+		if err != nil {
+			log.Fatalf("Failed to join auction: %v", err)
+			continue
+		}
+		log.Println("Current status:", response.Status)
+
+		if reflect.DeepEqual(responses[0], responses[1]) {
+			log.Println(responses[0].Status)
+		} else if reflect.DeepEqual(responses[0], responses[2]) {
+			log.Println(responses[0].Status)
+		} else if reflect.DeepEqual(responses[1], responses[2]) {
+			log.Println(responses[1].Status)
+		} else {
+			log.Println("Servers don't agree. Øv Bøv")
+		}
+	}
+}
+
+func getBid(client h5.AuctionserviceClient) {
+
 }
--- main.go
+++ main.go
@@ -6,14 +6,13 @@ import (
 	"sync"
 
 	client "github.com/DiSysCBFA/Handind-5/Client"
-	server "github.com/DiSysCBFA/Handind-5/Server"
 	"github.com/manifoldco/promptui"
 )
 
 func main() {
 	var wg sync.WaitGroup
-	ports := []int{4000, 4001, 4002}
-	maxRetries := len(ports) - 1
+	ports := []string{"localhost:4000", "localhost:4001", "localhost:4002"}
+	//maxRetries := len(ports) - 1
 
 	for {
 		selection := promptui.Select{
@@ -28,13 +27,21 @@ func main() {
 
 		if result == "Start Server" {
 			log.Println("Starting server...")
-			auctionServer := server.NewServer("")
-			auctionServer.Start(ports[0], maxRetries)
+			/* auctionServer := server.NewServer("")
+			auctionServer.Start(ports[0], maxRetries) */
 		} else if result == "Start Client" {
-			go client.StartClient(&wg, ports)
+
+			enterID := promptui.Prompt{}
+			enteredID, err := enterID.Run()
+			if err != nil {
+				log.Fatalf("Failed to enter ID: %v", err)
+			}
+			go client.StartClient(&wg, ports, enterID)
 		} else if result == "Exit" {
 			log.Println("Exiting...")
 			os.Exit(0)
 		}
+		wg.Wait()
 	}
+
 }
